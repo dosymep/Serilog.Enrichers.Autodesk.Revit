07541b1585881f96e872ef02f20b450ebcd67cc9
dosymep <dosymep@gmail.com>
2023-05-31 16:22:39 +0300

    added other configuration

--- src/Serilog.Enrichers.Autodesk.Revit/AutodeskRevitLoggerConfigurationExtensions.cs
+++ src/Serilog.Enrichers.Autodesk.Revit/AutodeskRevitLoggerConfigurationExtensions.cs
@@ -133,6 +133,60 @@ public static class AutodeskRevitLoggerConfigurationExtensions {
         return loggerEnrichmentConfiguration.WithRevitProperty(revitLanguagePropertyName,
             uiApplication.Application.Language.GetCultureInfo().EnglishName);
     }
+    
+    /// <summary>
+    /// Enrich log events with a <a href="https://www.revitapidocs.com/2017.1/4cee7916-d799-fc83-daf3-97cb03900b72.htm">Document.Title</a> property.
+    /// </summary>
+    /// <param name="loggerEnrichmentConfiguration">Logger enrichment configuration.</param>
+    /// <param name="uiApplication">Revit UIApplication.</param>
+    /// <param name="revitDocumentTitlePropertyName">Revit document title name. Default is <see cref="RevitDocumentTitlePropertyName"/>.</param>
+    /// <returns>Configuration object allowing method chaining.</returns>
+    public static LoggerConfiguration WithRevitDocumentTitle(this LoggerEnrichmentConfiguration loggerEnrichmentConfiguration,
+        UIApplication uiApplication,
+        string revitDocumentTitlePropertyName = RevitDocumentTitlePropertyName) {
+        return loggerEnrichmentConfiguration.With(new DocumentTitlePropertyEnricher(revitDocumentTitlePropertyName, uiApplication));
+    }
+    
+    /// <summary>
+    /// Enrich log events with a <a href="https://www.revitapidocs.com/2022/8a92a6fd-ce25-cd86-2068-f9dcb24d72d6.htm">Document.PathName</a> property.
+    /// </summary>
+    /// <param name="loggerEnrichmentConfiguration">Logger enrichment configuration.</param>
+    /// <param name="uiApplication">Revit UIApplication.</param>
+    /// <param name="revitDocumentPathNamePropertyName">Revit document path name. Default is <see cref="RevitDocumentPathNamePropertyName"/>.</param>
+    /// <returns>Configuration object allowing method chaining.</returns>
+    public static LoggerConfiguration WithRevitDocumentPathName(this LoggerEnrichmentConfiguration loggerEnrichmentConfiguration,
+        UIApplication uiApplication,
+        string revitDocumentPathNamePropertyName = RevitDocumentPathNamePropertyName) {
+        return loggerEnrichmentConfiguration.With(new DocumentPathNamePropertyEnricher(revitDocumentPathNamePropertyName, uiApplication));
+    }
+    
+    /// <summary>
+    /// Enrich log events with a ModelPath property.
+    /// <br/>If <a href="https://www.revitapidocs.com/2020/e12f7980-ba6c-2e72-6687-f0bf807ff014.htm">Document.IsModelInCloud</a> is true enriches <a href="https://www.revitapidocs.com/2020/087a7c14-1a6e-7022-c47b-923e90f4c5be.htm">Document.GetCloudModelPath()</a>.
+    /// <br/>If <a href="https://www.revitapidocs.com/2017.1/7f368167-6543-9be9-67a3-c6e1696ae060.htm">Document.IsWorkshared</a> is true enriches <a href="https://www.revitapidocs.com/2017.1/6d42ee05-5738-8685-2165-57f9809f3161.htm">Document.GetWorksharingCentralModelPath()</a>.
+    /// </summary>
+    /// <param name="loggerEnrichmentConfiguration">Logger enrichment configuration.</param>
+    /// <param name="uiApplication">Revit UIApplication.</param>
+    /// <param name="revitDocumentModelPathPropertyName">Revit document model path. Default is <see cref="RevitDocumentModelPathPropertyName"/>.</param>
+    /// <returns>Configuration object allowing method chaining.</returns>
+    public static LoggerConfiguration WithRevitDocumentModelPath(this LoggerEnrichmentConfiguration loggerEnrichmentConfiguration,
+        UIApplication uiApplication,
+        string revitDocumentModelPathPropertyName = RevitDocumentModelPathPropertyName) {
+        return loggerEnrichmentConfiguration.With(new DocumentModelPathPropertyEnricher(revitDocumentModelPathPropertyName, uiApplication));
+    }
+    
+    /// <summary>
+    /// Enrich log events with a <a href="https://www.revitapidocs.com/2017.1/08272796-d8b8-8122-c712-08b108184334.htm">ActiveAddinId</a> property.
+    /// </summary>
+    /// <param name="loggerEnrichmentConfiguration">Logger enrichment configuration.</param>
+    /// <param name="uiApplication">Revit UIApplication.</param>
+    /// <param name="revitAddinPropertyName">Revit adin id. Default is <see cref="RevitAddinPropertyName"/>.</param>
+    /// <returns>Configuration object allowing method chaining.</returns>
+    public static LoggerConfiguration WithRevitAddinId(this LoggerEnrichmentConfiguration loggerEnrichmentConfiguration,
+        UIApplication uiApplication,
+        string revitAddinPropertyName = RevitAddinPropertyName) {
+        return loggerEnrichmentConfiguration.With(new RevitAddinIdPropertyEnricher(revitAddinPropertyName, uiApplication));
+    }
 
     internal static LoggerConfiguration WithRevitProperty<T>(
         this LoggerEnrichmentConfiguration loggerEnrichmentConfiguration, string propertyName, T propertyValue) {
