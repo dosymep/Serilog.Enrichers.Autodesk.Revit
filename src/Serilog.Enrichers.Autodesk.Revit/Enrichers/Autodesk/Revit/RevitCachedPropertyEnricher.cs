ecb33f1c40f4869116ed7fc67d3e71b499c448d2
dosymep <dosymep@gmail.com>
2023-05-31 13:35:35 +0300

    add revit cached propery enricher

---
+++ src/Serilog.Enrichers.Autodesk.Revit/Enrichers/Autodesk/Revit/RevitCachedPropertyEnricher.cs
@@ -0,0 +1,26 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+// ReSharper disable once CheckNamespace
+namespace Serilog.Enrichers.Autodesk.Revit; 
+
+internal class RevitCachedPropertyEnricher<T> : ILogEventEnricher {
+    private readonly string _propertyName;
+    private readonly T _propertyValue;
+
+    private LogEventProperty? _logEventProperty;
+
+    public RevitCachedPropertyEnricher(string propertyName, T propertyValue) {
+        if(string.IsNullOrEmpty(propertyName)) {
+            throw new ArgumentException("Value cannot be null or empty.", nameof(propertyName));
+        }
+
+        _propertyName = propertyName;
+        _propertyValue = propertyValue;
+    }
+    
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory) {
+        _logEventProperty ??= propertyFactory.CreateProperty(_propertyName, _propertyValue);
+        logEvent.AddPropertyIfAbsent(_logEventProperty);
+    }
+}
